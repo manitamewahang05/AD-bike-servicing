e8e0aacd9fc5252f7abbb4d18957169340bdeee8
manitamewahang05 <55806574+manitamewahang05@users.noreply.github.com>
2020-07-31 00:56:46 +0545

    searchpackges

---
+++ models/Searchpacakges.model.js
@@ -0,0 +1,9 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const SearchpackagesSchema = new Schema({
+
+})
+
+
+module.exports = users = mongoose.model("Searchpackages", SearchpackagesSchema);
