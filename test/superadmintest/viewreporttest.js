3c07b5b030aac54e8ecd7699986c0534b03ef46b
manitamewahang05 <55806574+manitamewahang05@users.noreply.github.com>
2020-08-29 09:44:09 +0545

    viewreportsuperadmin

---
+++ test/superadmintest/viewreporttest.js
@@ -0,0 +1,23 @@
+// const request = require("supertest");
+// const expect = require("chai").expect;
+// const report = require("../models/ReportModel.model");
+// const app = require("../server");
+// const config = require("config");
+// const Mongoose = require("mongoose");
+// const ReportModel = require("../models/ReportModel.model");
+// const jwt = require("jsonwebtoken");
+
+// describe("VIEW ADMIN REPORT : GET/", () => {
+//     describe("RETRIEVE ADMIN REPORT ", () => {
+//         beforeEach(function(done) {
+//             this.timeout(9000);
+//             searchdetails.deleteMany({}, (err) => {
+//               if (err) return done(err);
+                
+//                 done();
+
+//             })
+//         });
+
+//     });
+// });
