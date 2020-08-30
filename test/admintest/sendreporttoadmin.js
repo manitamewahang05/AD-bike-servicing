95a5e4aa0b44ab6ec0b9f3113c04eea4689f33a4
manitamewahang05 <55806574+manitamewahang05@users.noreply.github.com>
2020-08-30 08:18:59 +0545

    sendreporttoadmin

--- test/admintest/sendreporttoadmin.js
+++ test/admintest/sendreporttoadmin.js
@@ -1,11 +1,11 @@
 const request = require("supertest");
 const expect = require("chai").expect;
-const feedbacks = require("../models/Report.model")
-const app = require("../server");
+const feedbacks = require("../../models/Report.model");
+const app = require("../../server");
 const Mongoose = require("mongoose");
 const { reporters } = require("mocha");
 
-describe("SEND REPORT : POST/", () => {
+describe("SEND  USERREPORT  TO SUPERADMIN  : POST/", () => {
     describe("SEND REPORT", () => {
         beforeEach(function(done) {
             this.timeout(9000);
@@ -19,9 +19,9 @@ describe("SEND REPORT : POST/", () => {
 
         it("When  message  is empty it should return :400 ", async() => {
             const report =new feedbacks( {
-                Name:"",
-                Email:"",
-               Message:"",
+                Name:" harry",
+                Email:"harry@gmail.com",
+               Message:" This was a good serice",
 
            
         });
@@ -29,7 +29,7 @@ describe("SEND REPORT : POST/", () => {
 
             // await user.save();
 
-            const res = await request(app).post("/sendfeedback/").send(report)
+            const res = await request(app).post("/sendfeedbacks/").send(report)
             expect(res.status).to.equal(400);
             expect(res.body.error[0].msg).to.equal("Please write some message");
 
@@ -45,7 +45,7 @@ describe("SEND REPORT : POST/", () => {
 
             // await user.save();
 
-            const res = await request(app).post("/sendfeedback/").send(report)
+            const res = await request(app).post("/sendfeedbacks/").send(report)
             expect(res.status).to.equal(400);
             expect(res.body.error[0].msg).to.equal("Please enter your  name");
 
@@ -62,7 +62,7 @@ describe("SEND REPORT : POST/", () => {
 
             // await user.save();
 
-            const res = await request(app).post("/sendfeedback/").send(report)
+            const res = await request(app).post("/sendfeedbacks/").send(report)
             expect(res.status).to.equal(400);
             expect(res.body.error[0].msg).to.equal("Please  enter your mail");
 
